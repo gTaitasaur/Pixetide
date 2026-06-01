8bfdf14960200ff49bd6cf3ef67f1eefda1fecf7
gTaitasaur <christian_loso@hotmail.com>
2026-06-01 16:47:21 -0400

    Home (No initMagickEngine) + Bing Meta Tag

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,6 @@ import App from './App.tsx'
 import './core/styles/variables.css'
 import './index.css'
 
-import { initMagickEngine } from './shared/utils/magickEngine';
-
-// Iniciamos la descarga en segundo plano inmediatamente
-initMagickEngine();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
